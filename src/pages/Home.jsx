6abfa9903207bf2628cdd0b541067be64c1b1962
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-07-15 22:41:52 +0300

    +

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import { getApi } from 'components/API/Api';
-import { useCallback, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const getTrending = 'trending/all/day';
 
@@ -8,18 +8,18 @@ const Home = () => {
   const [movies, setMovies] = useState([]);
   const location = useLocation();
 
-  const getRequest = useCallback(async () => {
-    try {
-      const { data } = await getApi(getTrending);
-      setMovies(data.results);
-    } catch (error) {
-      alert(error.message);
-    }
-  }, []);
-
   useEffect(() => {
+    const getRequest = async () => {
+      try {
+        const { data } = await getApi(getTrending);
+        setMovies(data.results);
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+
     getRequest();
-  }, [getRequest]);
+  }, []);
 
   return (
     <>
